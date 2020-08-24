30a967164b39837b9edf0e32d5e44a066f50c1c1
lachlanalston <41813866+lachlanalston@users.noreply.github.com>
2020-08-25 03:58:21 +1000

    Create weather.sh

---
+++ weather.sh
@@ -0,0 +1,12 @@
+#!/bin/dash
+# ------------------------------------------------------------------
+# Script Name   : SW
+# Description   : Simple Weather
+# Bugs          : 
+# Author        : Lachlan Alston
+# Dependency    :
+#
+# ------------------------------------------------------------------
+Version=0.1
+SUBJECT=
+USAGE= "Usage: sh weather.sh"
